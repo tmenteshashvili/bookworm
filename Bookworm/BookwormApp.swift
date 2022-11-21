917b75bbec80a801236733b0c8c3485ac8d65d2d
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-11-21 14:37:17 +0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Tako Menteshashvili on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
